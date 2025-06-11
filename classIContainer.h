422c7020feef97df03e7dddde5e9726d458e6ea4
EmelyanovPavel <gdetoty@list.ru>
2025-06-11 14:17:48 +0300

    Изменения в коде
    Класс интерфейса сделан шаблонным

--- classIContainer.h
+++ classIContainer.h
@@ -2,9 +2,10 @@
 #define CLASSICONTAINER_H_
 
 //// Container interface 
+template <typename T>
 class IContainer {
 public:
-    virtual void add(const void* data) = 0;
+    virtual void add(const T& data) = 0;
     virtual void remove() = 0;
     virtual bool isEmpty() const = 0;
     virtual ~IContainer() {} // Virtual destructor
